c637df4d10c4f3a406a4379e8a8f436be85c280e
Rajika-Kandege <54842482+Rajika-Kandege@users.noreply.github.com>
2024-05-16 22:06:30 +0800

    Create SimpleProgramme.java

---
+++ tests/SimpleProgramme.java
@@ -0,0 +1,14 @@
+public class SimpleProgramme {
+
+    public static void main(String[] args) {
+        printTheseInYourMachine();
+
+    }
+
+    public static void printTheseInYourMachine() {
+        System.out.println("Hi there");
+        System.out.println("I am on windows");
+        System.out.println("I am on macbook pro");
+        System.out.println("Hey suning");
+    }
+}
