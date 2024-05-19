e1b5142ee76209e4d048a8ba812568c120a54ce6
Rajika-Kandege <54842482+Rajika-Kandege@users.noreply.github.com>
2024-05-19 16:05:16 +0800

    Update SimpleProgramme.java

--- tests/SimpleProgramme.java
+++ tests/SimpleProgramme.java
@@ -4,6 +4,8 @@ public class SimpleProgramme {
 
     public static void main(String[] args) {
         printTheseInYourMachine();
+        sortNumbersInAnArray(new int[] {2,4,1,3,5,2,3,45,23});
+
 
     }
 
@@ -14,4 +16,11 @@ public class SimpleProgramme {
         System.out.println("I am on macbook pro");
         System.out.println("Hey suning");
     }
+
+     public static void sortNumbersInAnArray(int[] items){
+
+    Arrays.sort(items);
+    for(int eachNumber : items){
+      System.out.println(eachNumber);
+   }
 }
