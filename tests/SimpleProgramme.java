74ea288f515644ed3229bb7198982e42735383e0
Rajika-Kandege <54842482+Rajika-Kandege@users.noreply.github.com>
2024-05-23 23:22:12 +0800

    Update SimpleProgramme.java

--- tests/SimpleProgramme.java
+++ tests/SimpleProgramme.java
@@ -9,7 +9,7 @@ import com.jayway.jsonpath.JsonPath;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+
 
 public class SimpleProgramme {
 
